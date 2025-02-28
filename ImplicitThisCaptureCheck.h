98e0aeacba4eaf711f1c6d532e91f93a555b1f6a
Damien L-G <dalg24@gmail.com>
2025-02-28 17:21:32 -0500

    Add license and swap headers

--- ImplicitThisCaptureCheck.h
+++ ImplicitThisCaptureCheck.h
@@ -1,14 +1,18 @@
-//===--- ImplicitThisCaptureCheck.h - clang-tidy ----------------*- C++ -*-===//
-// 
-// Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC
-// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
-// Government retains certain rights in this software.
+//@HEADER
+// ************************************************************************
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===----------------------------------------------------------------------===//
+//@HEADER
 
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_KOKKOS_IMPLICITTHISCAPTURECHECK_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_KOKKOS_IMPLICITTHISCAPTURECHECK_H
